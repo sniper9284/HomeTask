0cda2459677a76ab21c5ec2a6d7b3e24190e5bba
sniper9284 <sniper-sochi@yandex.ru>
2021-04-07 17:01:55 +0300

    исправление ошибок и замечаний.

--- src/ru/synergy/hometask/outsqrt/OutSqrt.java
+++ src/ru/synergy/hometask/outsqrt/OutSqrt.java
@@ -8,14 +8,5 @@ public class OutSqrt {
         System.out.println("Введите число для извелечения корня");
         int usersqrt = scanner.nextInt();
 
-        if (usersqrt % 2 == 0) {
-            int outX;
-            do {
-                int x = usersqrt / 2;
-                 outX = x * x;
-            } while (outX < usersqrt);
-            System.out.println(outX);
-        }
     }
-
 }
